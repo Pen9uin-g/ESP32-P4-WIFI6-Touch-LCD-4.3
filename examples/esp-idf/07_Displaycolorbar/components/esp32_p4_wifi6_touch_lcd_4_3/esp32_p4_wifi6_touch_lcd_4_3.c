7ba97d1a14742b22c5e169fdd62a0db3190079cb
Y1hsiaochunnn <y1hsiaochunnn@qq.com>
2026-08-01 16:51:55 +0800

    fix: harden CI and align LCD formats

--- examples/esp-idf/07_Displaycolorbar/components/esp32_p4_wifi6_touch_lcd_4_3/esp32_p4_wifi6_touch_lcd_4_3.c
+++ examples/esp-idf/07_Displaycolorbar/components/esp32_p4_wifi6_touch_lcd_4_3/esp32_p4_wifi6_touch_lcd_4_3.c
@@ -507,9 +507,17 @@ esp_err_t bsp_display_new_with_handles(const bsp_display_config_t *config, bsp_l
         .dpi_clock_freq_mhz = 30,                     
         .virtual_channel = 0,                         
 #if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(6, 0, 0)
+#if CONFIG_BSP_LCD_COLOR_FORMAT_RGB888
+        .in_color_format = LCD_COLOR_FMT_RGB888,
+#else
         .in_color_format = LCD_COLOR_FMT_RGB565,
+#endif
+#else
+#if CONFIG_BSP_LCD_COLOR_FORMAT_RGB888
+        .pixel_format = LCD_COLOR_PIXEL_FORMAT_RGB888,
 #else
         .pixel_format = LCD_COLOR_PIXEL_FORMAT_RGB565,
+#endif
 #endif
         .num_fbs = 1,                                 
         .video_timing = {                             
